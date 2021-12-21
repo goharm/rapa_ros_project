d7d7a531420a94f0228f28e1198ae4a52678b43d
현주훈 <ceo@snmp.kr>
2021-12-21 14:10:44 +0900

    layout 구성

---
+++ main.py
@@ -0,0 +1,17 @@
+def func_01():
+    pass
+    return
+
+
+
+if __name__ == '__main__':
+    try:
+        pass
+
+    except Exception as e: 
+        pass
+    
+    finally:
+        pass
+    
+pass
