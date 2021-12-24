a0ddea3ba52ad4bb2abe115ad1174b74de20b53a
goharm <mrgoharm@gmail.com>
2021-12-24 15:17:49 +0900

    catkin_make

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ubuntu/Develops/rapa_ros_project/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ubuntu/Develops/rapa_ros_project/catkin_ws/devel;/home/ubuntu/Develops/ros1/rolling_ws/devel;/opt/ros/noetic'.split(';') if '/home/ubuntu/Develops/rapa_ros_project/catkin_ws/devel;/home/ubuntu/Develops/ros1/rolling_ws/devel;/opt/ros/noetic' != '' else []
---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH='/home/ubuntu/Develops/ros1/rolling_ws/devel/lib:/opt/ros/noetic/lib'
+export PWD='/home/ubuntu/Develops/rapa_ros_project/catkin_ws/build'
