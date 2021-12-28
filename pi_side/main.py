c0bfda27bc1b756be9ac8f0f162c778b2df77431
goharm <mrgoharm@gmail.com>
2021-12-28 09:36:34 +0900

    .

---
+++ pi_side/main.py
@@ -0,0 +1,21 @@
+import listener as ls
+
+def func_01():
+    pass
+    return
+
+
+
+if __name__ == '__main__':
+    try:
+        ls.listener()
+
+    except Exception as e: 
+        print(e)
+    
+    finally:
+        print("문제가 있다.")
+
+pass
+
+
